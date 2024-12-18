b7bb9087c2d0ea5c850ca3686b388576b75e3536
thienjs <thien.tran6@gmail.com>
2024-12-18 15:33:00 -0500

    David Do

--- app/components/animated-name.tsx
+++ app/components/animated-name.tsx
@@ -3,7 +3,7 @@ import { Link } from 'next-view-transitions';
 export function AnimatedName() {
   return (
     <Link href="/" className="flex mb-8 font-medium text-gray-400 fade-in">
-      Thien Tran
+      Dave Do
     </Link>
   );
 }
